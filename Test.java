e128751d7b34a3bd04bdc3111ab08c9d3d30e933
adoniscreedbalboa <12vatsanshu@gmail.com>
2023-06-16 12:52:27 +0530

    On branch Test

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Test test=new Test();
+        test.hg();
+    }
+public String hg(){
+    String test="this is a testing branch";
+    System.out.println("this is in log");
+    return test;
+}
+}
