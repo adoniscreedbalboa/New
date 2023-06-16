707b0cf906fe7e782bb9c147b58e8a33e3a1a9c0
adoniscreedbalboa <12vatsanshu@gmail.com>
2023-06-16 16:28:01 +0530

    On branch Fj

---
+++ Fj.java
@@ -0,0 +1,10 @@
+public class Fj {
+    public static void main(String[] args) {
+        Fj fj= new Fj();
+        fj.jdsl();
+    }
+
+    public void jdsl(){
+        System.out.println("this is fj branch");
+    }
+}
